b3e5e24ad082465489b19b5e62731a55aa527692
Larry Huang <larry.huang0220@gmail.com>
2016-10-25 13:47:56 +0800

    update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var index = require('./routes/index');
 //var users  = require('./routes/users');
 var account  = require('./routes/account');
 var profile  = require('./routes/profile');
+var photo = require('./routes/photo')
 
 //app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
@@ -21,6 +22,7 @@ app.use('/', index);
 //app.use('/users', users);
 app.use('/account', account);
 app.use('/profile', profile);
+app.use('/photo', photo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,12 +35,11 @@ app.use(function(req, res, next) {
 // no stacktraces leaked to user unless in development environment
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
-    error: (app.get('env') === 'development') ? err : {}
+    // error: (app.get('env') === 'development') ? err : {}
   });
 });
 
-//console.log('hihi');
 
 module.exports = app;
